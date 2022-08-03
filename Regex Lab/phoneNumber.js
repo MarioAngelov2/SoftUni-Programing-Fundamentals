52542db3cb37a273ea001ebc3a3de10a24a227cc
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2022-08-03 22:06:34 +0300

    Create phoneNumber.js

---
+++ Regex Lab/phoneNumber.js
@@ -0,0 +1,17 @@
+function solve(data) {
+
+    let pattern = /\+359([ |-])2\1\d{3}\1\d{4}/g
+    let phoneNumbersList = data[0];
+
+    let match = pattern.exec(phoneNumbersList);
+    let result = [];
+
+    while (match !== null) {
+        result.push(match[0]);
+
+        match = pattern.exec(phoneNumbersList);
+    }
+
+console.log(result.join(', '))
+}
+solve(['+359 2 222 2222,359-2-222-2222, +359/2/222/2222, +359-2 222 2222 +359 2-222-2222, +359-2-222-222, +359-2-222-22222 +359-2-222-2222'])
